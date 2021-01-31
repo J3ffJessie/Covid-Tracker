9ed87cb75ddfe98d2dd5f6e76de1c01093419cb6
J3ffJessie <jejess6125@gmail.com>
2021-01-31 10:59:19 -0600

    Add react-dropdown for dropdown component usage, input all 50 states with abbreviations, instantiate the component onto the homepage, add button for submission of selected state to the API call.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import ApiCall from './covidApi'
+import PickState from "../components/dropdown"
 
 const IndexPage = () => (
   <Layout>
@@ -12,7 +13,8 @@ const IndexPage = () => (
     <h1>Welcome to the Site!</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-      <ApiCall />
+      <PickState />
+      {/* <ApiCall /> render only if the user has selected a state from the dropdown menu otherwise render the main page with dropdown selection and submit button */}
     </div>
   </Layout>
 )
