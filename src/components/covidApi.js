4f1f79f0583c0ba5685e1aafbbd7a491cc18e025
j3ffjessie <jejess6125@gmail.com>
2021-02-09 11:23:53 -0600

    Stats in Detail/ Summary area, made Charts area the same. Style changes, verified API functionality.  Need to rewrite test to verify that it functions correctly.

--- src/components/covidApi.js
+++ src/components/covidApi.js
@@ -1,43 +1,58 @@
 import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
-import axios from 'axios'
-import states from '../utils/states'
+import axios from "axios"
+import states from "../utils/states"
 
 import Layout from "./layout"
 import SEO from "./seo"
 
-
 export default function ApiCall() {
-  const [requestState, setRequestState] = useState('idle')
+  const [requestState, setRequestState] = useState("idle")
 
-  const [data, setData] = useState('')
+  const [data, setData] = useState("")
 
-  const [state, setState] = useState('il');
+  const [state, setState] = useState("tn")
 
-  const onStateSelect = (e) => {
-    e.preventDefault();
+  const onStateSelect = e => {
+    e.preventDefault()
     setState(e.target.value)
   }
 
   async function fetchCovidData(value) {
-    const response = await axios.get(`https://api.covidtracking.com/v1/states/${value}/current.json`);
-    setData(await response.data);
-    console.log(response.data);
-
+    const response = await axios.get(
+      `https://api.covidtracking.com/v1/states/${value}/current.json`
+    )
+    setData(await response.data)
+    console.log(response.data)
   }
 
   useEffect(() => {
-    fetchCovidData(state);
-  }, [state]);
+    fetchCovidData(state)
+  }, [state])
 
   if (!data) {
-    return "loading....";
-  }else {
+    return "loading...."
+  } else {
     return (
       <div>
-        <div className='data'>
+        <div className="data">
           <div className="banner">
-            <div className="container">
+            <div className="drop-down">
+              <select
+                name="state"
+                id="stateSelect"
+                onChange={e => onStateSelect(e)}
+              >
+                {states.map(s => (
+                  <option value={s.value}>{s.label}</option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </div>
+        <details>
+          <summary>Show Statistics</summary>
+          <div className="container">
             <h1>Covid Data</h1>
             <h2>Here are the stats for the State chosen: {data.state} </h2>
             <p>Date: {data.date}</p>
@@ -47,25 +62,23 @@ export default function ApiCall() {
             <p># of Patients Hospitalized: {data.hospitalizedCurrently}</p>
             <p># of Patients in ICU Currently: {data.inIcuCurrently}</p>
             <p>Last Update: {data.lastUpdateEt}</p>
-            <p>Increased Positive Tests from Last Update: {data.positiveIncrease}</p>
-            <p>Increased Negative Tests from Last Update: {data.negativeIncrease}</p>
+            <p>
+              Increased Positive Tests from Last Update: {data.positiveIncrease}
+            </p>
+            <p>
+              Increased Negative Tests from Last Update: {data.negativeIncrease}
+            </p>
             <p># of Deaths since Last Update: {data.deathIncrease}</p>
             <p># of increased hospitilizations: {data.hospitalizedIncrease}</p>
-            </div>
           </div>
-        </div>
-        <div>
-          <select name='state' id='stateSelect' onChange={(e) => onStateSelect(e)}>
-            {
-              states.map(s => <option value={s.value}>{s.label}</option>)
-            }
-          </select>
-        </div>
+        </details>
+        <details className='chart-container'>
+          <summary>Show Charts</summary>
+          <div className='charts'>
+            
+          </div>
+        </details>
       </div>
-  
-    )    
+    )
   }
-
-  }
-
-  
+}
