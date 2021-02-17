366a5753967fc656f27932453b49ba1225cd15f0
andrew-t-james <ajames20@gmail.com>
2021-02-16 19:34:50 -0600

    ✅ Testing the things

--- src/components/covidApi.js
+++ src/components/covidApi.js
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useRef } from "react"
 import { Link } from "gatsby"
 import axios from "axios"
 import states from "../utils/states"
-import NumberFormat from 'react-number-format'
+import NumberFormat from "react-number-format"
 
 import Layout from "./layout"
 import SEO from "./seo"
 
 export default function ApiCall() {
+  const firstUpdate = useRef(false)
   const [requestState, setRequestState] = useState("idle")
 
   const [data, setData] = useState("")
@@ -23,7 +24,7 @@ export default function ApiCall() {
     const response = await axios.get(
       `https://api.covidtracking.com/v1/states/${value}/current.json`
     )
-    setData(await response.data)
+    setData(response.data)
   }
 
   useEffect(() => {
@@ -41,7 +42,9 @@ export default function ApiCall() {
               onChange={e => onStateSelect(e)}
             >
               {states.map(s => (
-                <option value={s.value}>{s.label}</option>
+                <option key={s.value} value={s.value}>
+                  {s.label}
+                </option>
               ))}
             </select>
           </div>
@@ -52,70 +55,128 @@ export default function ApiCall() {
         <summary>Show Statistics</summary>
         <div className="container">
           <table>
-            <tr>
-              <th className="table-title">Category</th>
-              <th className="table-title">{data.state}'s Statistics </th>
-            </tr>
-            {data && data.lastUpdateEt ? (
+            <thead>
               <tr>
-                <td>Date/Time of Last Update</td>
-                <td>{data.lastUpdateEt}</td>
+                <th className="table-title">Category</th>
+                <th className="table-title">{data.state}'s Statistics </th>
               </tr>
-            ) : null}
-            {data && data.positive ? (
-              <tr>
-                <td>Positive Results</td>
-                <td><NumberFormat value={data.positive} displayType={'text'} thousandSeparator={true}/></td>
-              </tr>
-            ) : null}
-            {data && data.negative ? (
-              <tr>
-                <td>Negative Results</td>
-                <td><NumberFormat value={data.negative} displayType={'text'} thousandSeparator={true}/></td>
-              </tr>
-            ) : null}
-            {data && data.totalTestResults ? (
-              <tr>
-                <td>Total Test Results</td>
-                <td><NumberFormat value={data.totalTestResults} displayType={'text'} thousandSeparator={true}/></td>
-              </tr>
-            ) : null}
-            {data && data.hospitalizedCurrently ? (
-              <tr>
-                <td># of Patients Currently Hospitalized</td>
-                <td><NumberFormat value={data.hospitalizedCurrently} displayType={'text'} thousandSeparator={true}/></td>
-              </tr>
-            ) : null}
-            {data && data.inIcuCurrently ? (
-              <tr>
-                <td># of Patients in ICU Currently</td>
-                <td><NumberFormat value={data.inIcuCurrently} displayType={'text'} thousandSeparator={true}/></td>
-              </tr>
-            ) : null}
-            {data && data.positiveIncrease ? (
-              <tr>
-                <td>Increase in Positive Results from Last Update</td>
-                <td><NumberFormat value={data.positiveIncrease} displayType={'text'} thousandSeparator={true}/></td>
-              </tr>
-            ) : null}
-            {data && data.negativeIncrease ? (
-              <tr>
-                <td>Increase in Negative Results from Last Update</td>
-                <td><NumberFormat value={data.negativeIncrease} displayType={'text'} thousandSeparator={true}/></td>
-              </tr>
-            ) : null}
-            {data && data.deathIncrease ? (
-              <tr>
-                <td># of Deaths since Last Update</td>
-                <td><NumberFormat value={data.deathIncrease} displayType={'text'} thousandSeparator={true}/></td>
-              </tr>
-            ) : null}
-            {data && data.hospitalizedIncrease ? (
-              <tr>
-                <td># of Increased Hospitalizations</td>
-                <td><NumberFormat value={data.hospitalizedIncrease} displayType={'text'} thousandSeparator={true}/></td>
-              </tr>
-            ) : null}
+            </thead>
+            <tbody>
+              {data && data.lastUpdateEt ? (
+                <tr>
+                  <td>Date/Time of Last Update</td>
+                  <td>{data.lastUpdateEt}</td>
+                </tr>
+              ) : null}
+              {data && data.positive ? (
+                <tr>
+                  <td>Positive Results</td>
+                  <td>
+                    <NumberFormat
+                      value={data.positive}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                    />
+                  </td>
+                </tr>
+              ) : null}
+              {data && data.negative ? (
+                <tr>
+                  <td>Negative Results</td>
+                  <td>
+                    <NumberFormat
+                      value={data.negative}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                    />
+                  </td>
+                </tr>
+              ) : null}
+              {data && data.totalTestResults ? (
+                <tr>
+                  <td>Total Test Results</td>
+                  <td>
+                    <NumberFormat
+                      value={data.totalTestResults}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                    />
+                  </td>
+                </tr>
+              ) : null}
+              {data && data.hospitalizedCurrently ? (
+                <tr>
+                  <td># of Patients Currently Hospitalized</td>
+                  <td>
+                    <NumberFormat
+                      value={data.hospitalizedCurrently}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                    />
+                  </td>
+                </tr>
+              ) : null}
+              {data && data.inIcuCurrently ? (
+                <tr>
+                  <td># of Patients in ICU Currently</td>
+                  <td>
+                    <NumberFormat
+                      value={data.inIcuCurrently}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                    />
+                  </td>
+                </tr>
+              ) : null}
+              {data && data.positiveIncrease ? (
+                <tr>
+                  <td>Increase in Positive Results from Last Update</td>
+                  <td>
+                    <NumberFormat
+                      value={data.positiveIncrease}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                    />
+                  </td>
+                </tr>
+              ) : null}
+              {data && data.negativeIncrease ? (
+                <tr>
+                  <td>Increase in Negative Results from Last Update</td>
+                  <td>
+                    <NumberFormat
+                      value={data.negativeIncrease}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                    />
+                  </td>
+                </tr>
+              ) : null}
+              {data && data.deathIncrease ? (
+                <tr>
+                  <td># of Deaths since Last Update</td>
+                  <td>
+                    <NumberFormat
+                      value={data.deathIncrease}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                    />
+                  </td>
+                </tr>
+              ) : null}
+              {data && data.hospitalizedIncrease ? (
+                <tr>
+                  <td># of Increased Hospitalizations</td>
+                  <td>
+                    <NumberFormat
+                      value={data.hospitalizedIncrease}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                    />
+                  </td>
+                </tr>
+              ) : null}
+            </tbody>
           </table>
         </div>
       </details>
--- test/covidApi.test.js
+++ test/covidApi.test.js
@@ -1,8 +1,7 @@
 import React from "react"
-import { render, act, waitFor } from "@testing-library/react"
+import { render, waitFor, act, screen } from "@testing-library/react"
 import axios from "axios"
 import ApiCall from "../src/components/covidApi"
-import renderer from "react-test-renderer"
 
 jest.mock("axios")
 
@@ -15,51 +14,33 @@ const fakeData = {
   totalTestResults: "45",
   hostpitalizedCurrently: "0",
   inIcuCurrently: "0",
-  lastUpdateEt: "2/1/2021 01:00",
+  lastUpdateEt: "2/1/2021 11:00",
   positiveIncrease: "10",
   negativeIncrease: "0",
   deathIncrease: "0",
   hospitalizedIncrease: "0",
 }
 
+beforeEach(() => {
+  axios.get.mockImplementationOnce(() => Promise.resolve({ data: fakeData }))
+})
+
 it("renders covid statistics loading state", async () => {
-  const { asFragment } = render(<ApiCall />)
-  expect(asFragment()).toMatchSnapshot()
+  await waitFor(() => {
+    const { asFragment } = render(<ApiCall />)
+    expect(asFragment()).toMatchSnapshot()
+  })
 })
 
 it("should render covid statistics", async () => {
-  axios.get.mockImplementationOnce(() => Promise.resolve(fakeData))
-
-  // had to do this because of scope
-  let container
   await waitFor(() => {
-    const utils = render(<ApiCall />)
-    container = utils.container
+    render(<ApiCall />)
   })
-  expect(container.querySelectorAll("p")[0].textContent).toBe(
-    `Here are the stats for the State chosen: ${fakeData.state} `
-  )
-  expect(container.querySelectorAll("p")[1].textContent).toBe(
-    `Date: ${fakeData.date}`
-  )
-  // expect(container.querySelector("p").textContent).toBe(fakeData.positive)
-  // expect(container.querySelector("p").textContent).toBe(fakeData.negative)
-  // expect(container.querySelector("p").textContent).toBe(
-  //   fakeData.totalTestResults
-  // )
-  // expect(container.querySelector("p").textContent).toBe(
-  //   fakeData.hostpitalizedCurrently
-  // )
-  // expect(container.querySelector("p").textContent).toBe(fakeData.inIcuCurrently)
-  // expect(container.querySelector("p").textContent).toBe(fakeData.lastUpdateEt)
-  // expect(container.querySelector("p").textContent).toBe(
-  //   fakeData.positiveIncrease
-  // )
-  // expect(container.querySelector("p").textContent).toBe(
-  //   fakeData.negativeIncrease
-  // )
-  // expect(container.querySelector("p").textContent).toBe(fakeData.deathIncrease)
-  // expect(container.querySelector("p").textContent).toBe(
-  //   fakeData.hospitalizedIncrease
-  // )
+
+  expect(
+    screen.getByRole("cell", { name: /date\/time of last update/i }).textContent
+  ).toEqual("Date/Time of Last Update")
+  expect(
+    screen.getByRole("cell", { name: /2\/1\/2021 11:00/i }).textContent
+  ).toEqual(fakeData.lastUpdateEt)
 })
